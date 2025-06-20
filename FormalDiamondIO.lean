fa3870cba81e4b5da81ce861d7566d556cddcbd8
quantumshiro <hiro.1nakanishi@gmail.com>
2025-06-20 13:09:36 +0900

    add: modules

--- FormalDiamondIO.lean
+++ FormalDiamondIO.lean
@@ -1,3 +1,5 @@
 -- This module serves as the root of the `FormalDiamondIO` library.
 -- Import modules here that should be built as part of the library.
 import FormalDiamondIO.Basic
+import FormalDiamondIO.LWE
+import FormalDiamondIO.DiamondIO
